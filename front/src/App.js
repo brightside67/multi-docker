b93180b8407427bcc60508a2897c75cdb3497045
brightside67 <mobrika@list.ru>
2019-05-05 12:30:04 +0300

    added front instead of client foleder

---
+++ front/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header>React app</header>
+        <div>
+          <Link to="/">Home!</Link>
+          <Link to="/otherpage">Othe rpage</Link>
+        </div>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route exact path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
